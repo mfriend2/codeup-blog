a81b402d77be7a41e70ea44eff452afe09e19f79
Michael Friend <michael.l.friend2@gmail.com>
2022-08-02 11:31:29 -0500

    update

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,27 +1,24 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
 
-    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    @GetMapping(value = "/posts")
     @ResponseBody
     public String index() {return "<h1>Posts index page</h1>";}
 
-    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/posts/{id}")
     @ResponseBody
     public String onePost() {return "<h1>View an individual post</h1>";}
 
-    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
+    @GetMapping(value = "/posts/create")
     @ResponseBody
     public String createView() {return "<h1>View the form for creating a post</h1>";}
 
-    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    @PostMapping(value = "/posts/create")
     @ResponseBody
     public String createPost() {return "<h1>Create a new post</h1>";}
 }

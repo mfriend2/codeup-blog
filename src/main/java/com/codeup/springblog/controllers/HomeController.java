b9e6e1f017f194228a1b7305d90d39a4b03ad567
Michael Friend <michael.l.friend2@gmail.com>
2022-08-02 14:28:54 -0500

    lecture follow

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,12 +1,9 @@
 package com.codeup.springblog.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
-    public String welcome() {return "<h3>This is the landing page!</h3>";}
+    public String welcome() {return "home";}
 }

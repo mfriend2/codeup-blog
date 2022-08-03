1ffcb4ba693834dba52d4f47d1d70c8d1c6413a9
Michael Friend <michael.l.friend2@gmail.com>
2022-08-03 16:24:14 -0500

    lecture follow along

--- src/main/java/com/codeup/springblog/models/Product.java
+++ src/main/java/com/codeup/springblog/models/Product.java
@@ -13,5 +13,43 @@ public class Product {
     private String name;
 
     @Column(nullable = false)
-    private double price;
+    private String price;
+
+    public Product() {
+    }
+
+    public Product(long id, String name, String price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
---
+++ src/main/java/com/codeup/springblog/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

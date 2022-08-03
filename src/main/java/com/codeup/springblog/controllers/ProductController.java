ea04961a066fdf325dd324ecfce7073dbaadaca3
Michael Friend <michael.l.friend2@gmail.com>
2022-08-03 16:47:57 -0500

    lecture follow along

--- src/main/java/com/codeup/springblog/controllers/ProductController.java
+++ src/main/java/com/codeup/springblog/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Product;
 import com.codeup.springblog.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -17,7 +20,12 @@ public class ProductController {
     @GetMapping("/products")
     public String all(Model model) {
         model.addAttribute("products", productsDao.findAll());
-        System.out.println(productsDao.getProductByName("Oil - Safflower"));
+
+        List<Product> searchedProducts = productsDao.searchByNameLike("oil");
+        for (Product product : searchedProducts) {
+            System.out.println(product.getName());
+        }
+        System.out.println(productsDao.getProductByName("Oil - Safflower").getName());
         return "products/index";
     }
 }
--- src/main/java/com/codeup/springblog/repositories/ProductRepository.java
+++ src/main/java/com/codeup/springblog/repositories/ProductRepository.java
@@ -2,7 +2,13 @@ package com.codeup.springblog.repositories;
 
 import com.codeup.springblog.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product getProductByName(String name);
+    @Query("from Product product where product.name like %:name%")
+    List<Product> searchByNameLike(@Param("name") String name);
 }

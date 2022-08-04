bbaf6b9dbe8953f7f929a39c87bef2fa82f096ba
Michael Friend <michael.l.friend2@gmail.com>
2022-08-04 10:26:08 -0500

    completed jpa exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,41 +12,34 @@ import java.util.List;
 
 @Controller
 public class PostController {
-    private Post post = new Post(1, "Hello", "This is a new post.");
-    private Post post2 = new Post(2, "Bye", "This is the last post.");
-    private List<Post> posts = new ArrayList<Post>();
+    private PostRepository postsDao;
 
-    public PostController() {
-        posts.add(post);
-        posts.add(post2);
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
     }
 
     @GetMapping(value = "/posts")
     public String index(Model model) {
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping(value = "/posts/{id}")
     public String onePost(@PathVariable long id, Model model) {
-        model.addAttribute("posts", posts);
-        model.addAttribute("id", id);
+        model.addAttribute("post", postsDao.getById(id));
         return "posts/show";
     }
 
     @GetMapping(value = "/posts/create")
-    @ResponseBody
     public String createView() {
-        return "<h1>View the form for creating a post</h1>";
+        return "posts/create";
     }
 
     @PostMapping(value = "/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "<h1>Create a new post</h1>";
+    public String createPost(@RequestParam("title") String title, @RequestParam("body") String body) {
+        Post newPost = new Post(title, body);
+        postsDao.save(newPost);
+        return "redirect:/posts";
     }
 
-    public static void main(String[] args) {
-
-    }
 }
---
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}

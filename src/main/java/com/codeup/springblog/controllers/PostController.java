9d09d34f2af67c87c2da9f44aefe44a81453ffc5
Michael Friend <michael.l.friend2@gmail.com>
2022-08-05 10:07:32 -0500

    completed relationships exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,7 +1,9 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +15,11 @@ import java.util.List;
 @Controller
 public class PostController {
     private PostRepository postsDao;
+    private UserRepository usersDao;
 
-    public PostController(PostRepository postsDao) {
+    public PostController(PostRepository postsDao, UserRepository usersDao) {
         this.postsDao = postsDao;
+        this.usersDao = usersDao;
     }
 
     @GetMapping(value = "/posts")
@@ -37,9 +41,10 @@ public class PostController {
 
     @PostMapping(value = "/posts/create")
     public String createPost(@RequestParam("title") String title, @RequestParam("body") String body) {
-        Post newPost = new Post(title, body);
+        User newUser = new User("new", "new@email.com", "password");
+        Post newPost = new Post(title, body, newUser);
+        usersDao.save(newUser);
         postsDao.save(newPost);
         return "redirect:/posts";
     }
-
 }
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -15,6 +15,10 @@ public class Post {
     @Column(nullable = false)
     private String body;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public Post() {
     }
 
@@ -27,6 +31,19 @@ public class Post {
         this.id = id;
         this.title = title;
         this.body = body;
+    }
+
+    public Post(long id, String title, String body, User user) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+
+    public Post(String title, String body, User user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
     }
 
     public long getId() {
@@ -51,5 +68,13 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
